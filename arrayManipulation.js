651503756517d7e5e40fddc49a2e091a67901a92
y-Mutawakil <myussif020@st.ug.edu.gh>
2024-05-17 19:48:09 +0000

    Adding Task Two

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,3 +15,19 @@ function processArray(arr) {
 
 
 
+// Task Two
+
+  // Function to format array of strings based on processed numbers
+  function formatArrayStrings(stringsArr, numbersArr) {
+    return stringsArr.map((str, index) => {
+      const num = numbersArr[index];
+      if (num % 2 === 0) {
+        // Capitalize string if the number is even
+        return str.toUpperCase();
+      } else {
+        // Convert string to lowercase if the number is odd
+        return str.toLowerCase();
+      }
+    });
+  }
+  
